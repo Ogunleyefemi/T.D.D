817e4a5eb8c9e342f92c489342d316ba785522bb
femi <oluwafemiogunleye21@gmail.com>
2022-08-28 06:43:08 -0700

    add logic to check if its a number

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
 function wordCounter(text){
     // to check for empty string
-    if (text.length === 0){
+    if (text.trim().length === 0){
         return 0;
     }
     let Counter = 0;
-    const wordArr = word.split(" ");
+    const wordArr = text.split(" ");
     wordArr.forEach(function(word) {
+        // logic tocheck for number
+        if(!Number(word)){
         Counter++;
+        }
     })
     return Counter;
 };
