04c7f819f4535fb7b6b3cbab502b6ef2fe66a35f
femi <oluwafemiogunleye21@gmail.com>
2022-08-28 07:08:23 -0700

    add logic to check for empty string and spaces

--- script.js
+++ script.js
@@ -15,6 +15,9 @@
 // };
 // to check number of occurences in a text
 function numberOfOccurrencesInText(word, text) {
+    if (text.trim().length === 0){
+        return 0
+    }
     const wordArray = text.split(" ");
     let wordCount = 0;
     wordArray.forEach(function(element) {
