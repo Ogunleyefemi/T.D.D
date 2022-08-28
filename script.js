ee35adccc2166388932ddb5bf3b9a5d3627c06fb
femi <oluwafemiogunleye21@gmail.com>
2022-08-28 05:48:10 -0700

    add logic to check for empty string

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 function wordCounter(text){
+    // to check for empty string
+    if (text.length === 0){
+        return 0;
+    }
     let Counter = 0;
     const wordArr = word.split(" ");
     wordArr.forEach(function(word) {
