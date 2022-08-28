3766d87c3b8a5c1309e80321365f0e36df0f31bd
femi <oluwafemiogunleye21@gmail.com>
2022-08-28 06:55:17 -0700

    add number of occurences in a text

--- script.js
+++ script.js
@@ -1,15 +1,26 @@
-function wordCounter(text){
-    // to check for empty string
-    if (text.trim().length === 0){
-        return 0;
-    }
-    let Counter = 0;
-    const wordArr = text.split(" ");
-    wordArr.forEach(function(word) {
-        // logic tocheck for number
-        if(!Number(word)){
-        Counter++;
-        }
-    })
-    return Counter;
-};
+// function wordCounter(text){
+//     // to check for empty string
+//     if (text.trim().length === 0){
+//         return 0;
+//     }
+//     let Counter = 0;
+//     const wordArr = text.split(" ");
+//     wordArr.forEach(function(word) {
+//         // logic tocheck for number
+//         if(!Number(word)){
+//         Counter++;
+//         }
+//     })
+//     return Counter;
+// };
+// to check number of occurences in a text
+function numberOfOccurrencesInText(word, text) {
+    const wordArray = text.split(" ");
+    let wordCount = 0;
+    wordArray.forEach(function(element) {
+      if (word === element) {
+        wordCount++
+      }
+    });
+    return wordCount;
+  }
